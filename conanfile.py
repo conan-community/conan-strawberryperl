fab48e68386d448e69422f6ceec27da067241d89
Luis Martinez de Bartolome <lasote@gmail.com>
2017-12-16 02:17:36 +0100

    short paths

--- conanfile.py
+++ conanfile.py
@@ -9,6 +9,7 @@ class StrawberryperlConan(ConanFile):
     url = "https://github.com/lasote/conan-strawberryperl"
     settings = "os", "arch"
     description = "Strawbery Perl for Windows. Useful as build_require"
+    short_paths = True
 
     def configure(self):
         if self.settings.os != "Windows":
@@ -16,12 +17,12 @@ class StrawberryperlConan(ConanFile):
 
     def build(self):
         installer = {"x86": "strawberry-perl-5.26.0.1-32bit-portable.zip",
-                     "x86_64": "strawberry-perl-5.26.0.1-64bit-portable.zip"}[str(self.settings.arch)]    
+                     "x86_64": "strawberry-perl-5.26.0.1-64bit-portable.zip"}[str(self.settings.arch)]
         url = "http://strawberryperl.com/download/5.26.0.1/%s" % installer
         tools.download(url, filename="perl.zip")
         tools.unzip("perl.zip")
         os.unlink("perl.zip")
-        
+
     def package(self):
         self.copy("*", keep_path=True)
         self.copy("licenses/License.rtf*", dst=".", keep_path=False, ignore_case=True)
@@ -29,4 +30,4 @@ class StrawberryperlConan(ConanFile):
     def package_info(self):
         self.env_info.PATH.append(os.path.join(self.package_folder, "perl", "bin"))
         self.env_info.PATH.append(os.path.join(self.package_folder, "c", "bin"))
-        
+
