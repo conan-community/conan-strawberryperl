0be86100d33cc3bb79cb892abc2805ad4b5b34f0
Luis Martinez de Bartolome <lasote@gmail.com>
2017-12-21 16:59:51 +0100

    Prepared for cross settings in Conan 1.0

--- build.py
+++ build.py
@@ -3,6 +3,6 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add({"os": "Windows", "arch":"x86_64"}, {}, {}, {})
-    builder.add({"os": "Windows", "arch":"x86"}, {}, {}, {})
+    builder.add({"os": "Windows", "arch": "x86_64"}, {}, {}, {})
+    builder.add({"os": "Windows", "arch": "x86"}, {}, {}, {})
     builder.run()
--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,6 @@
 from conans import ConanFile, tools
+from conans import __version__ as conan_version
+from conans.model.version import Version
 import os
 
 
@@ -7,17 +9,29 @@ class StrawberryperlConan(ConanFile):
     version = "5.26.0"
     license = "GNU Public License or the Artistic License."
     url = "https://github.com/lasote/conan-strawberryperl"
-    settings = "os", "arch"
+    if conan_version < Version("1.0.0"):
+        settings = {"os": ["Windows", "Linux", "Macos"],
+                    "arch": ["x86", "x86_64"]}
+    else:
+        settings = "os_build", "arch_build"
     description = "Strawbery Perl for Windows. Useful as build_require"
     short_paths = True
 
+    @property
+    def arch(self):
+        return self.settings.get_safe("arch_build") or self.settings.get_safe("arch")
+
+    @property
+    def os(self):
+        return self.settings.get_safe("os_build") or self.settings.get_safe("os")
+
     def configure(self):
-        if self.settings.os != "Windows":
+        if self.os != "Windows":
             raise Exception("Only windows supported for strawberry perl")
 
     def build(self):
         installer = {"x86": "strawberry-perl-5.26.0.1-32bit-portable.zip",
-                     "x86_64": "strawberry-perl-5.26.0.1-64bit-portable.zip"}[str(self.settings.arch)]
+                     "x86_64": "strawberry-perl-5.26.0.1-64bit-portable.zip"}[str(self.arch)]
         url = "http://strawberryperl.com/download/5.26.0.1/%s" % installer
         tools.download(url, filename="perl.zip")
         tools.unzip("perl.zip")
