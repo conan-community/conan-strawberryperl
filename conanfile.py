39b348218ce6ea5bca7ee2e14357ccb0afafb60a
Luis Martinez de Bartolome <lasote@gmail.com>
2017-12-27 13:54:55 +0100

    0.99

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class StrawberryperlConan(ConanFile):
     version = "5.26.0"
     license = "GNU Public License or the Artistic License"
     url = "https://github.com/lasote/conan-strawberryperl"
-    if conan_version < Version("1.0.0"):
+    if conan_version < Version("0.99"):
         settings = "os", "arch"
     else:
         settings = "os_build", "arch_build"
